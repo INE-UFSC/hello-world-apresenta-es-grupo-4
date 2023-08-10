b4583aec118fda578437d471709a8daccbfbe299
Victória Rodrigues Veloso <victoria.rvv@outlook.com>
2023-08-10 15:33:40 -0300

    apresentação victória

--- membro2.py
+++ membro2.py
@@ -1,2 +1,6 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return """Olá, eu sou a Victória Rodrigues Veloso, tenho 21 anos. Meus hobbies são ir para a academia, praia, trilhas e coisas relacionadas a natureza. Também gosto de experimentar fazer
+	receitas novas, mas cozinhas por obrigação é algo que me deixa irritada. Minha experiência com programação é básica, sei o que aprendi na UFSC e durante as férias comecei um curso para aprender C++ e sites em flask.
+	Espero que nessa disciplina eu consiga desenvolver mais as minhas habilidades e adquirir conhecimentos para criar novos projetos individuais.
+
+	""" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
